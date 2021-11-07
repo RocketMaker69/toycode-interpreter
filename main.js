afcbd5590e6374829c3311c9dc22d37a9ace8a00
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-11-07 11:12:41 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+let interpreter = { };
+
+Object.defineProperties(interpreter, {
+    stack: { value: [ ], writable: true },
+    load: { value: function (val) {
+        this.stack.push(val) } 
+    },
+    add: { value: function () {
+        let b = this.stack.pop();
+        let a = this.stack.pop();
+        this.load((a + b)); }
+    },
+    run: { value: function (c) {
+        let [ins, dat] = [c.ins, c.dat];
+        for (let step in ins) {
+            let [i, a] = step;
+            match (i) {
+                case "load":
+                    this.load(a);
+                    break;
+                case "add":
+                    this.add();
+                    break;
+                case "print":
+                    if (a === null) console.log(this.stack.pop());
+                    else            console.log(dat[a]);
+                    break;
+            }
+        }
+    }
+});
